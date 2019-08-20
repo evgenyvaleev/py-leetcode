85625e3496904eeb77f522fb28df09a02f8825f5
Evgeny Valeev <evgenyvlv@gmail.com>
2019-08-20 10:54:51 +0300

    Add "Plus One"

---
+++ plus_one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        shift = 1
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] = digits[i] + shift
+            if digits[i] > 9:
+                digits[i] -= 10
+                shift = 1
+            else:
+                shift = 0
+        
+        if shift > 0:
+            digits = [shift] + digits
+        
+        return digits
